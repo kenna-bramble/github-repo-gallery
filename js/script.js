d949cdc70fc83e68830ed155466ab7ee00de7739
kenna_b <hello@kennabramble.com>
2021-12-02 10:00:40 -0500

    Wrote JS to fetch API JSON data for Github Profile info, fetched and displayed user info, fetched repos, displayed info about repos

--- js/script.js
+++ js/script.js
@@ -2,11 +2,15 @@
 const overview = document.querySelector(".overview"); 
 const username = "kenna-bramble";
 
+//targets <ul> to display the repos list
+const repoList = document.querySelector(".repo-list");
+
+
 //Fetch API JSON Data for GitHub profile info
 const gitUserInfo = async function () {
     const userInfo = await fetch (`https://api.github.com/users/${username}`);
     const data = await userInfo.json();
-    console.log(data);
+    //console.log(data);
     displayUserInfo(data);
 };
 gitUserInfo();
@@ -17,6 +21,23 @@ const displayUserInfo = function (data) {
     div.classList.add("user-info");
     div.innerHTML = `<figure><img alt="user avatar" src=${data.avatar_url} /></figure><div><p><strong>Name:</strong> ${data.name}</p><p><strong>Bio:</strong> ${data.bio}</p><p><strong>Location:</strong> ${data.location}</p><p><strong>Number of public repos:</strong> ${data.public_repos}</p></div>`;
     overview.append(div);
+    gitRepos();
 };
 
-//Call the Display Function & View Your Project
+//Fetch Repos, YEAH!
+const gitRepos = async function () {
+    const fetchRepos = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    displayRepos(repoData);
+};
+
+
+//Display Info About Your Repos
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
+};
